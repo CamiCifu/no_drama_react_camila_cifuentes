d83c3cd1733db4c138e384315aec284fe0ba6e0b
CamiCifu <camiscifu@gmail.com>
2022-11-09 13:20:15 -0300

    adicion de cards productos

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -7,9 +7,9 @@ const Item = ({ producto }) => {
   return (
     <Link to={`/detalle/${producto.id}`}>
       <div key={producto.id} style={{ marginLeft: 100 }} className="col-md-6">
-        <div className=" card d-flex  p-2 w-18 mt-10 mb-10 ">
+        <div className=" card d-flex  p-2 w-18 mt-10 mb-10 item__textoSpace ">
           <img src={producto.foto} alt="" className="w-50 card-img-top" />{" "}
-          <div class="card-body justify-content-center">
+          <div class="item__text card-body justify-content-center">
             <div className="card-title">{`${producto.nombre} - ${producto.categoria}`}</div>
             <div className="card-text">{producto.precio}</div>
             <div className="card-footer"></div>
